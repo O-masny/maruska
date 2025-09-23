2515f895fb72d2cc6e4190823fa28ac2d0b78ab0
ondrej masny <iicelyy@seznam.cz>
2025-09-23 23:21:28 +0200

    qwe

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
 
 Route::get('/', function () {
     return Inertia::render('Index');
