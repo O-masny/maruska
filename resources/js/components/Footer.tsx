f0d3413c1284f39f15a702bf8c77f9ae395da377
ondrej masny <iicelyy@seznam.cz>
2025-10-08 14:45:26 +0200

    fx

--- resources/js/components/Footer.tsx
+++ resources/js/components/Footer.tsx
@@ -41,7 +41,7 @@ const Footer = () => {
 
     return (
         <footer className="bg-card border-t border-border">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                     {/* Logo & Description */}
                     <div className="lg:col-span-1">
